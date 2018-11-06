b35c951ef0226873da5d90029166e0cba1204f0a
jbrolin <brolin.jonas@gmail.com>
2018-11-06 11:57:03 +0100

    added api endpoint

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express(); 
 var port = 8081; 
 app.listen(port);
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
 
-var taskList = []; 
+var taskList=[{'task1':'one'}];
+app.post('/task', function(req, res){ 
+    taskList.push(req.body.task);   
+res.send(taskList); 
+})
+app.delete('/task/:task_index', function(req, res){   
+    var taskIndex = req.params.task_index;   
+    taskList.splice(taskIndex, 1);   
+    res.send(taskList); 
+})
+
+
 app.get('/list', function(req, res){   
     res.send(taskList); 
 })
