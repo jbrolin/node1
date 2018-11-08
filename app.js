e5151a5d972fcea9873a252c0542f83a873a012e
jbrolin <brolin.jonas@gmail.com>
2018-11-08 20:02:24 +0100

    added lodash

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ app.listen(port);
 var bodyParser = require('body-parser'); 
 app.use(bodyParser.json());
 
+var _  = require('lodash');
+
 var taskList=[{'task1':'one'}];
-app.post('/task', function(req, res){ 
-    taskList.push(req.body.task);   
-res.send(taskList); 
-})
+
 app.delete('/task/:task_index', function(req, res){   
     var taskIndex = req.params.task_index;   
     taskList.splice(taskIndex, 1);   
@@ -21,5 +20,20 @@ app.get('/list', function(req, res){
     res.send(taskList); 
 })
 
-console.log("Server is listening on", port) 
+app.put('/task/:task_index', function(req, res){   
+    var taskIndex = req.params.task_index;   
+    taskList[taskIndex] = req.body.task;  
+    res.send(taskList); 
+})
+
+app.post('/task', function(req, res){ 
+    var task = req.body.task;   
+    if(_.isEmpty(task)){     
+        return res.status(422).send("Task is empty");   
+    }
+    taskList.push(req.body.task);   
+    res.send(taskList); 
+})
+
+console.log("Server is listening on", port); 
 
